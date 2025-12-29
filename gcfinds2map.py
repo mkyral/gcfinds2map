32332c22aff8cbc7d1e515cf7ca0570160f1f512
Marián Kyral <mkyral@email.cz>
2025-12-29 08:31:22 +0100

    Allow read gpx from zip archive

--- gcfinds2map.py
+++ gcfinds2map.py
@@ -13,6 +13,8 @@ More info on
 """
 
 import sys
+import os.path
+import zipfile
 import geopandas as gpd
 #import geodatasets
 import folium
@@ -22,34 +24,59 @@ import matplotlib.pyplot as plt
 program_name = sys.argv[0]
 arguments = sys.argv[1:]
 
+# Check number of arguments
 if (len(arguments) < 1):
-    print("Usage: %s my_finds_pocket_query_gpx_file" % (program_name))
+    print("Usage: %s <my_finds_file>" % (program_name))
     exit(1)
 
-my_finds_gpx = arguments[0]
 
-# Load data
-# Czech Republic contour (admin_level=2)
-cr_map = gpd.read_file("data/cr.geojson")
+# Verify that file exists
+if (not os.path.isfile(arguments[0])):
+    print("Error: %s is not a file" % arguments[0])
+    exit(1)
 
-# Czech Republic division (admin_level=4)
-kraje = gpd.read_file("data/cr_kraje.geojson")
+#
+# Load data files
+#
 
 # Geocache finds (pocket query)
-gpx = gpd.read_file(my_finds_gpx)
+my_finds_gpx = arguments[0]
+
+if (len(arguments) > 1):
+    out_file = arguments[1]
+else:
+    out_file = "my_finds.png"
+
+# Load from zip archive
+if (zipfile.is_zipfile(my_finds_gpx)):
+    try:
+        archive = zipfile.ZipFile(my_finds_gpx, 'r')
+        gpx = gpd.read_file(archive.open(archive.namelist()[0]))
+    except:
+        print("Error: Can't open zip file!")
+        exit(1)
+else: # Load as gpx file
+    gpx = gpd.read_file(my_finds_gpx)
+
+# Czech Republic contour (admin_level=2)
+contour = gpd.read_file("data/cr.geojson")
+
+# Czech Republic division (admin_level=4)
+division = gpd.read_file("data/cr_kraje.geojson")
+
 
 # Ensure coordinates system is the same
-gpx = gpx.to_crs(cr_map.crs)
+gpx = gpx.to_crs(contour.crs)
 
 # Limit finds to Czech Republic border box
-xmin, ymin, xmax, ymax = cr_map.total_bounds
+xmin, ymin, xmax, ymax = contour.total_bounds
 filtered_gpx = gpx.cx[xmin:xmax, ymin:ymax]
 
 # Plot grey divisions
-base = kraje.plot(figsize=(10, 7), color="none", edgecolor="darkgrey", markersize=0)
+base = division.plot(figsize=(10, 7), color="none", edgecolor="darkgrey", markersize=0)
 
 # Plot contour
-base = cr_map.plot(ax=base, figsize=(10, 7), color="none", edgecolor="black")
+base = contour.plot(ax=base, figsize=(10, 7), color="none", edgecolor="black")
 
 # Plot finds
 filtered_gpx.plot(ax=base, marker="*", color='red', markersize=1);
@@ -58,7 +85,9 @@ filtered_gpx.plot(ax=base, marker="*", color='red', markersize=1);
 plt.axis('off')
 
 # Save image, remove axis padding
-plt.savefig("nalezy.png", bbox_inches='tight', pad_inches=0)
+plt.savefig(out_file, bbox_inches='tight', pad_inches=0)
+
+print("File %s generated" % out_file)
 
 # Show generated plot
 #plt.show()
